bef8994850307bd19daf7c75c570ddfb956b94a5
Xiaonan Hu <xiaonan.hu17@gmail.com>
2026-04-02 12:25:27 -0700

    add cache bust

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ cd ~/jubu-deploy
 source ~/.github_token
 git pull
 echo "Building..."
-docker compose build --no-cache --build-arg GITHUB_TOKEN=$GITHUB_TOKEN
+docker compose build --build-arg GITHUB_TOKEN=$GITHUB_TOKEN --build-arg CACHE_BUST=$(date +%s)
 echo "Starting..."
 docker compose up -d
 docker compose restart nginx
